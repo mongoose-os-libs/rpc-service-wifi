141e5da27b520f6e958721878c9d06ab220ec8ab
Dmitry Frank <mail@dmitryfrank.com>
2017-08-17 12:23:19 +0100

    Add RPC WiFi service with `Wifi.Scan` method
    PUBLISHED_FROM=d633a713252f48d6dfdf5946803b675847e0ba1e

---
+++ src/mgos_rpc_service_wifi.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#include <stdlib.h>
+
+#include "common/json_utils.h"
+#include "mgos.h"
+#include "mgos_rpc.h"
+
+static int wifi_scan_result_printer(struct json_out *out, va_list *ap) {
+  int len = 0;
+  int num_res = va_arg(*ap, int);
+
+  const struct mgos_wifi_scan_result *res =
+      va_arg(*ap, const struct mgos_wifi_scan_result *);
+
+  for (int i = 0; i < num_res; i++, res++) {
+    if (i > 0) len += json_printf(out, ", ");
+    len +=
+        json_printf(out, "{ssid: %Q, auth: %d, channel: %d, rssi: %d}",
+                    res[i].ssid, res[i].auth_mode, res[i].channel, res[i].rssi);
+  }
+
+  return len;
+}
+
+static void wifi_scan_cb(int n, struct mgos_wifi_scan_result *res, void *arg) {
+  struct mg_rpc_request_info *ri = (struct mg_rpc_request_info *) arg;
+
+  if (n < 0) {
+    mg_rpc_send_errorf(ri, n, "wifi scan failed");
+    return;
+  }
+  mg_rpc_send_responsef(ri, "{results: [%M]}", wifi_scan_result_printer, n,
+                        res);
+}
+
+static void mgos_rpc_wifi_scan_handler(struct mg_rpc_request_info *ri,
+                                       void *cb_arg,
+                                       struct mg_rpc_frame_info *fi,
+                                       struct mg_str args) {
+  if (!fi->channel_is_trusted) {
+    mg_rpc_send_errorf(ri, 403, "unauthorized");
+    ri = NULL;
+    return;
+  }
+
+  mgos_wifi_scan(wifi_scan_cb, ri);
+
+  (void) args;
+  (void) cb_arg;
+}
+
+bool mgos_rpc_service_wifi_init(void) {
+  struct mg_rpc *c = mgos_rpc_get_global();
+  mg_rpc_add_handler(c, "Wifi.Scan", "", mgos_rpc_wifi_scan_handler, NULL);
+
+  return true;
+}
